85f0c15de9acabce348f09c747ce348666587d23
linsadry <adriana_lins@hotmail.com>
2026-06-27 20:17:51 -0300

    Update image.js

--- src/utils/image.js
+++ src/utils/image.js
@@ -1,16 +1,17 @@
-export const compressImage = (file, maxW=1024, quality=0.85) => new Promise(res => {
-  const r = new FileReader();
-  r.onload = e => {
-    const img = new Image();
-    img.onload = () => {
-      const scale  = Math.min(1, maxW / img.width);
-      const canvas = document.createElement("canvas");
-      canvas.width  = img.width  * scale;
-      canvas.height = img.height * scale;
-      canvas.getContext("2d").drawImage(img, 0, 0, canvas.width, canvas.height);
-      res(canvas.toDataURL("image/jpeg", quality));
+export const compressImage = (file, maxW = 1024, quality = 0.85) =>
+  new Promise((res) => {
+    const r = new FileReader();
+    r.onload = (e) => {
+      const img = new Image();
+      img.onload = () => {
+        const scale  = Math.min(1, maxW / img.width);
+        const canvas = document.createElement("canvas");
+        canvas.width  = img.width  * scale;
+        canvas.height = img.height * scale;
+        canvas.getContext("2d").drawImage(img, 0, 0, canvas.width, canvas.height);
+        res(canvas.toDataURL("image/jpeg", quality));
+      };
+      img.src = e.target.result;
     };
-    img.src = e.target.result;
-  };
-  r.readAsDataURL(file);
-});
+    r.readAsDataURL(file);
+  });
