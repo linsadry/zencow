1d008c3dd1e1fadad10e012b0c4593cd8454a2dc
linsadry <adriana_lins@hotmail.com>
2026-06-28 02:13:33 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'globalThis',
-  },
-  optimizeDeps: {
-    include: ['@supabase/supabase-js'],
-    esbuildOptions: {
-      define: { global: 'globalThis' },
-    },
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
 })
