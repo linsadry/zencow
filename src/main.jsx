4f57ef74da6eed70387f61769bea8910a32bb7c4
linsadry <adriana_lins@hotmail.com>
2026-06-27 21:09:45 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import ZenCow from './src/ZenCow.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ZenCow />
   </React.StrictMode>
 );
